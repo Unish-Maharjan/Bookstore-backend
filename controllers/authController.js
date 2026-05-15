ef980a930ebece48c50a16f5043e9ba5e2002151
Unish Maharjan <uniqueunish93@gmail.com>
2026-05-15 23:20:05 +0545

    admin role added

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,20 +13,32 @@ const generateToken = (user) => {
 // POST /auth/register
 const register = async (req, res) => {
   try {
-    const { name, email, password } = req.body;
+    const { name, email, password, role } = req.body;
 
     const existingUser = await User.findOne({ email });
     if (existingUser)
       return res.status(400).json({ message: "Email already in use" });
 
-    const user = await User.create({ name, email, password });
-    res.status(201).json({ token: generateToken(user) });
+    const user = await User.create({
+      name,
+      email,
+      password,
+      role: role || "user",
+    });
+
+    res.status(201).json({
+      token: generateToken(user),
+      user: {
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
 
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
-
 // POST /auth/login
 const login = async (req, res) => {
   try {
